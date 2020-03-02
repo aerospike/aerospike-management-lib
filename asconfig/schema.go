4a6eb4ace921d8ad77184b945925927ac2c91951
sud82 <sudhanshu.ranjan.82@gmail.com>
2020-03-01 22:54:26 -0800

    Added initFromMap func

--- asconfig/schema.go
+++ asconfig/schema.go
@@ -56,6 +56,15 @@ func Init(schemaDir string) error {
 	return nil
 }
 
+// InitFromMap init schema map from a map.
+// Map key format -> 4.1.0
+// Map value format -> string of json schema
+func InitFromMap(schemaMap map[string]string) {
+	for name, schema := range schemaMap {
+		schemas[name] = schema
+	}
+}
+
 func versionFormat(filename string) string {
 	if len(filename) == 0 {
 		return ""
