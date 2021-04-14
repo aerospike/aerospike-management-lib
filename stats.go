82c9c425ad34193f2cfe80f3b3e2c6db9dee1d24
Ashish Shinde <ashishshinde@users.noreply.github.com>
2021-04-14 23:51:31 +0530

    Using reprint for deepcopy of unexported struct fields (#6)

--- stats.go
+++ stats.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"sync"
 
-	"github.com/jinzhu/copier"
+	"github.com/qdm12/reprint"
 )
 
 const NOT_SUPPORTED = "N/S"
@@ -653,5 +653,5 @@ func ToStatsDeep(input Stats) Stats {
 
 // DeepCopy Make a deep copy from src into dst. src, dst both should be pointer
 func DeepCopy(dst interface{}, src interface{}) error {
-	return copier.CopyWithOption(dst, src, copier.Option{DeepCopy: true, IgnoreEmpty: false})
+	return reprint.FromTo(src, dst)
 }
