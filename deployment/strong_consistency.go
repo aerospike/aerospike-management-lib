532f7cf2e66bb25ff13ef08b57560f3313cd26f0
Tanmay Jain <tjain@aerospike.com>
2024-09-05 13:13:52 +0530

    principal spell revert

--- deployment/strong_consistency.go
+++ deployment/strong_consistency.go
@@ -249,7 +249,7 @@ func recluster(clHost *host) error {
 
 	clHost.log.V(1).Info("Run info command", "cmd", cmd, "output", cmdOutput)
 
-	if !strings.EqualFold(cmdOutput, "ok") && !strings.EqualFold(cmdOutput, "ignored-by-non-pricipal") {
+	if !strings.EqualFold(cmdOutput, "ok") && !strings.EqualFold(cmdOutput, "ignored-by-non-principal") {
 		return fmt.Errorf("failed to run `%s` for cluster, %v", cmd, cmdOutput)
 	}
 
