b2b0505ba4c73cc5f2b6f7e6752f54395d1df857
Akash Chandra <achandra@aerospike.com>
2024-09-13 10:15:57 +0530

    fix: TOOLS-2971 fix in existing failing test cases (#56)
    * fix: Fix existing test cases
    
    * fix: Change the Test Cases Expects rather than Generated Values
    
    * fix: move the ConfigDCContext out for more readibilty
    
    * Revert "fix: move the ConfigDCContext out for more readibilty"
    
    This reverts commit 1769dfc4533621c2f1dbd8232add97cf079965e1.
    
    * fix: Same Fix for ConfigNamespaceContext

--- info/as_parser.go
+++ info/as_parser.go
@@ -1148,7 +1148,7 @@ func parseAllXDRConfig(rawMap map[string]string, cmd string) lib.Stats {
 
 	if dcNamesRaw == "" {
 		dcNames = []string{}
-		xdrConfigMap[ConfigDCContext] = struct{}{}
+		xdrConfigMap[ConfigDCContext] = make(lib.Stats)
 	} else {
 		dcNames = strings.Split(dcNamesRaw, ",")
 		xdrConfigMap[ConfigDCContext] = make(lib.Stats, len(dcNames))
@@ -1170,7 +1170,7 @@ func parseAllXDRConfig(rawMap map[string]string, cmd string) lib.Stats {
 
 		if nsNamesRaw == "" {
 			nsNames = []string{}
-			dcMap[ConfigNamespaceContext] = struct{}{}
+			dcMap[ConfigNamespaceContext] = make(lib.Stats)
 		} else {
 			nsNames = strings.Split(nsNamesRaw, ",")
 			dcMap[ConfigNamespaceContext] = make(lib.Stats, len(nsNames))
