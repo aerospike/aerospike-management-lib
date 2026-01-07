96b54248f7494e8becc95829082453cd457343e6
Akash Chandra <achandra@aerospike.com>
2026-01-07 11:03:01 -0800

    test: Refactor mock connection expectations in as_parser_test.go

--- info/as_parser_test.go
+++ info/as_parser_test.go
@@ -138,7 +138,20 @@ func (s *AsParserTestSuite) TestAsInfoGetAsConfigXDREnabled() {
 	s.mockConn.EXPECT().RequestInfo([]string{"get-config:context=xdr"}).Return(map[string]string{"get-config:context=xdr": "dcs=DC1,DC2;src-id=0;trace-sample=0"}, nil)
 	s.mockConn.EXPECT().RequestInfo([]string{"get-config:context=xdr;dc=DC1"}).Return(map[string]string{"get-config:context=xdr;dc=DC1": "auth-mode=none;auth-password-file=null;auth-user=null;connector=false;max-recoveries-interleaved=0;node-address-port=;period-ms=100;tls-name=null;use-alternate-access-address=false;namespaces=test"}, nil)
 	s.mockConn.EXPECT().RequestInfo([]string{"get-config:context=xdr;dc=DC2"}).Return(map[string]string{"get-config:context=xdr;dc=DC2": "auth-mode=none;auth-password-file=null;auth-user=null;connector=false;max-recoveries-interleaved=0;node-address-port=;period-ms=100;tls-name=null;use-alternate-access-address=false;namespaces=bar"}, nil)
-	s.mockConn.EXPECT().RequestInfo([]string{"get-config:context=xdr;dc=DC2;namespace=bar", "get-config:context=xdr;dc=DC1;namespace=test"}).Return(map[string]string{"get-config:context=xdr;dc=DC1;namespace=test": "enabled=true;bin-policy=all;compression-level=1;compression-threshold=128;delay-ms=0;enable-compression=false;forward=false;hot-key-ms=100;ignored-bins=", "get-config:context=xdr;dc=DC2;namespace=bar": "enabled=true;bin-policy=all;compression-level=1;compression-threshold=128;delay-ms=0;enable-compression=false;forward=false;hot-key-ms=100;ignored-bins="}, nil)
+	s.mockConn.EXPECT().RequestInfo(gomock.Any()).DoAndReturn(
+		func(cmds ...string) (map[string]string, aero.Error) {
+			expectedCmds := []string{
+				"get-config:context=xdr;dc=DC1;namespace=test",
+				"get-config:context=xdr;dc=DC2;namespace=bar",
+			}
+			s.ElementsMatch(expectedCmds, cmds)
+
+			return map[string]string{
+				"get-config:context=xdr;dc=DC1;namespace=test": "enabled=true;bin-policy=all;compression-level=1;compression-threshold=128;delay-ms=0;enable-compression=false;forward=false;hot-key-ms=100;ignored-bins=",
+				"get-config:context=xdr;dc=DC2;namespace=bar":  "enabled=true;bin-policy=all;compression-level=1;compression-threshold=128;delay-ms=0;enable-compression=false;forward=false;hot-key-ms=100;ignored-bins=",
+			}, nil
+		},
+	)
 
 	expected := lib.Stats{"xdr": lib.Stats{
 		"src-id":       int64(0),
